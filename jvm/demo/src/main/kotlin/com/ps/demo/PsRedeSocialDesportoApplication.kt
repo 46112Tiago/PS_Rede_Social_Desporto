d18872cec7a64fdbebdce0bdcaaa9ae234bb06af
46112Tiago <tiagoflalves11@gmail.com>
2022-03-30 00:10:32 +0100

    Setup Spring boot

---
+++ jvm/demo/src/main/kotlin/com/ps/demo/PsRedeSocialDesportoApplication.kt
@@ -0,0 +1,11 @@
+package com.ps.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PsRedeSocialDesportoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PsRedeSocialDesportoApplication>(*args)
+}
---
+++ jvm/demo/src/test/kotlin/com/ps/demo/PsRedeSocialDesportoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ps.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PsRedeSocialDesportoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
