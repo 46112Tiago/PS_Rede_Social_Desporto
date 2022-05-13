3f8d752bca43cb7817a7ffa50b8e4b65ad60ee2a
46112Tiago <tiagoflalves11@gmail.com>
2022-05-13 17:09:59 +0100

    Fix auth0 403

--- js/rede-social-desporto/src/Components/Auth0/ChooseAuthPath.js
+++ js/rede-social-desporto/src/Components/Auth0/ChooseAuthPath.js
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
-import Fetch from './Fetch';
 
 const ChooseAuthPath = () => {
   
 
     
         const {user, isAuthenticated} = useAuth0()
-        const {getAccessTokenWithPopup} = useAuth0();
+        const {getAccessTokenWithPopup,getAccessTokenSilently} = useAuth0();
 
           const [userArray, setUser] = useState([{}]);
-          const myHeaders = new Headers()
 
           useEffect(() => {
             (async () => {
               try {
-                const token = await getAccessTokenWithPopup();
-                myHeaders.append('Authorization', `Bearer ${token}`)
-                myHeaders.append('Content-Type', `application/json`)
-                myHeaders.append('Access-Control-Allow-Origin', 'https://localhost:3000');
-                myHeaders.append('Access-Control-Allow-Credentials', 'true');
+                const token = await getAccessTokenSilently();
+
                 if(isAuthenticated) {
-                  axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
-                  axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-                  axios.defaults.headers.get['Authorization'] = `Bearer ${token}`
-                  const response = await axios.get('http://localhost:8080/user');
+
+                  var options = {
+                    method: 'GET',
+                    url: 'http://localhost:8080/user',
+                    headers: {authorization: `Bearer ${token}`}
+                  };
+                  
+                  const response = await axios.request(options);
                   
                   setUser(response.data);
                 }
@@ -34,7 +33,7 @@ const ChooseAuthPath = () => {
                 alert(e);
               }
             })();
-          }, [getAccessTokenWithPopup]);
+          }, [getAccessTokenSilently]);
 
           if (!userArray) {
             return <div>Loading...</div>;
