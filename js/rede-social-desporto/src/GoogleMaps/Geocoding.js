2dfe2d1476cd6798fab2334a42636d00e36a4430
Dev Diogo Fernandes <diogotag@gmail.com>
2022-05-24 02:10:34 +0100

    Update Geocoding.js

--- js/rede-social-desporto/src/GoogleMaps/Geocoding.js
+++ js/rede-social-desporto/src/GoogleMaps/Geocoding.js
@@ -1,7 +1,7 @@
 import Geocode from "react-geocode";
 
 export function convertLocationToCoordinate(location) {
-    Geocode.setApiKey("AIzaSyCajrJ3Y0s5ubSgMAw8Gq34ujyt2TbJqi8")
+    Geocode.setApiKey("")
     Geocode.enableDebug();
     Geocode.fromAddress(location).then(
         (response) => {
@@ -15,7 +15,7 @@ export function convertLocationToCoordinate(location) {
 }
 
 export function convertCoordinateToLocation(lat,lng) {
-    Geocode.setApiKey("AIzaSyCajrJ3Y0s5ubSgMAw8Gq34ujyt2TbJqi8") //AIzaSyCajrJ3Y0s5ubSgMAw8Gq34ujyt2TbJqi8
+    Geocode.setApiKey("") 
     Geocode.enableDebug();
     Geocode.fromLatLng(lat, lng).then(
         (response) => {
